89f37e627f2315a8de980ef51b2dd1dbf36fb39f
Lucas Parra <lucasparra996@gmail.com>
2022-02-06 15:26:45 -0300

    nav casi listo

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,5 @@
-const menu = document.querySelector(".nav__menu--ul");
-const openMenuBtn = document.querySelector(".open_nav__menu");
-const closeMenuBtn = document.querySelector(".close_nav__menu");
-
-function toggleMenu(){
-  menu.classList.toggle("menu_opened");
-}
-
-openMenuBtn.addEventListener("click", toggleMenu);
-closeMenuBtn.addEventListener("click", toggleMenu);
+const btnMenu = document.querySelector("#btnMenu");
+const menu = document.querySelector("#menu");
+btnMenu.addEventListener("click", function(){
+  menu.classList.toggle("mostrarMenu");
+});
